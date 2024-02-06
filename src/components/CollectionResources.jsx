9fd2a0a3f1f5311362f4b9b1b8be4b861660d539
Małgorzata Kwiecień <kwiecien.malgorzata.00@wp.pl>
2024-02-06 18:13:57 +0100

    extracted collectionInfo to separate file

--- src/components/CollectionResources.jsx
+++ src/components/CollectionResources.jsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 
 import styled from 'styled-components'
-import { fetchCollectionResources } from '@/utils/http/fetchCollectionResources'
+import { fetchCollectionResources } from '@utils/http/fetchCollectionResources'
 
 import Container from '@components/ui/Container'
 import GridContainer from '@components/ui/GridContainer'
@@ -10,38 +10,7 @@ import { OpacityMotionContainer } from '@components/ui/MotionContainer'
 import Loader from '@components/ui/Loader'
 import ErrorBlock from '@components/ui/ErrorBlock'
 
-const collectionInfo = {
-	disney: {
-		fetchQuery: 'discover/movie?with_companies=2',
-		mediaType: 'movie',
-		collectionHeader: 'Explore Disney Collection',
-	},
-	pixar: {
-		fetchQuery: 'discover/movie?with_companies=3',
-		mediaType: 'movie',
-		collectionHeader: 'Explore Pixar Collection',
-	},
-	marvel: {
-		fetchQuery: 'discover/movie?with_companies=420',
-		mediaType: 'movie',
-		collectionHeader: 'Explore Marvel Collection',
-	},
-	starwars: {
-		fetchQuery: 'search/movie?query=star%20wars',
-		mediaType: 'movie',
-		collectionHeader: 'Explore Star Wars Collection',
-	},
-	nationalgeographic: {
-		fetchQuery: 'discover/movie?with_companies=7521',
-		mediaType: 'movie',
-		collectionHeader: 'Explore National Geographic Collection',
-	},
-	star: {
-		fetchQuery: 'discover/tv?with_networks=2739',
-		mediaType: 'series',
-		collectionHeader: 'Explore Star Collection',
-	},
-}
+import { collectionInfo } from '@utils/resources/collection-info'
 
 const CollectionResourcesPage = () => {
 	const params = useParams()
---
+++ src/utils/resources/collection-info.js
@@ -0,0 +1,32 @@
+export const collectionInfo = {
+	disney: {
+		fetchQuery: 'discover/movie?with_companies=2',
+		mediaType: 'movie',
+		collectionHeader: 'Explore Disney Collection',
+	},
+	pixar: {
+		fetchQuery: 'discover/movie?with_companies=3',
+		mediaType: 'movie',
+		collectionHeader: 'Explore Pixar Collection',
+	},
+	marvel: {
+		fetchQuery: 'discover/movie?with_companies=420',
+		mediaType: 'movie',
+		collectionHeader: 'Explore Marvel Collection',
+	},
+	starwars: {
+		fetchQuery: 'search/movie?query=star%20wars',
+		mediaType: 'movie',
+		collectionHeader: 'Explore Star Wars Collection',
+	},
+	nationalgeographic: {
+		fetchQuery: 'discover/movie?with_companies=7521',
+		mediaType: 'movie',
+		collectionHeader: 'Explore National Geographic Collection',
+	},
+	star: {
+		fetchQuery: 'discover/tv?with_networks=2739',
+		mediaType: 'series',
+		collectionHeader: 'Explore Star Collection',
+	},
+}
