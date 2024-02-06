cf4ba0c58f4b89a003c8f28bbcff0c230cf9ef2f
Małgorzata Kwiecień <kwiecien.malgorzata.00@wp.pl>
2024-02-06 18:07:29 +0100

    simplified DiscoverResource file

--- src/components/DiscoverResource.jsx
+++ src/components/DiscoverResource.jsx
@@ -10,39 +10,7 @@ import { OpacityMotionContainer } from '@components/ui/MotionContainer'
 import Loader from '@components/ui/Loader'
 import ErrorBlock from '@components/ui/ErrorBlock'
 import Title from '@components/ui/Title'
-
-const categoryOptions = {
-	movie: [
-		{ id: 28, name: 'Action' },
-		{ id: 12, name: 'Adventure' },
-		{ id: 16, name: 'Animation' },
-		{ id: 35, name: 'Comedy' },
-		{ id: 80, name: 'Crime' },
-		{ id: 99, name: 'Documentary' },
-		{ id: 18, name: 'Drama' },
-		{ id: 10751, name: 'Family' },
-		{ id: 14, name: 'Fantasy' },
-		{ id: 27, name: 'Horror' },
-		{ id: 9648, name: 'Mystery' },
-		{ id: 10749, name: 'Romance' },
-		{ id: 878, name: 'Science Fiction' },
-		{ id: 53, name: 'Thriller' },
-	],
-	series: [
-		{ id: 10759, name: 'Action & Adventure' },
-		{ id: 16, name: 'Animation' },
-		{ id: 35, name: 'Comedy' },
-		{ id: 80, name: 'Crime' },
-		{ id: 99, name: 'Documentary' },
-		{ id: 18, name: 'Drama' },
-		{ id: 10751, name: 'Family' },
-		{ id: 10762, name: 'Kids' },
-		{ id: 9648, name: 'Mystery' },
-		{ id: 10763, name: 'News' },
-		{ id: 10764, name: 'Reality' },
-		{ id: 10765, name: 'Sci-Fi & Fantasy' },
-	],
-}
+import { categoryOptions } from '@utils/resources/category-options'
 
 const DiscoverResource = ({ type }) => {
 	const storedCategory = localStorage.getItem(`selectedCategory_${type}`)
@@ -78,7 +46,7 @@ const DiscoverResource = ({ type }) => {
 				</StyledSelect>
 			</Wrapper>
 
-			<div>
+			<>
 				{isPending && <Loader />}
 				{isError && <ErrorBlock message='Something went wrong, please try again later.' />}
 				{isSuccess && !data.length && <p>No data available for the selected category.</p>}
@@ -87,7 +55,7 @@ const DiscoverResource = ({ type }) => {
 						<GridContainer movies={resourceList} path={type} />
 					</OpacityMotionContainer>
 				)}
-			</div>
+			</>
 		</StyledContainer>
 	)
 }
